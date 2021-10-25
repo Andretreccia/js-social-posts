a4770bf7e1fed1ddfa7e8e2039b835979349b3fa
Andretreccia <andreabtreccia@gmail.com>
2021-10-25 15:55:56 +0200

    Creazione struttura post

---
+++ assets/main.js
@@ -0,0 +1,40 @@
+/* Creare una pagina che elenchi una lists di post socials a partire da un
+ array di oggetti.
+Gli attributi minimi del modello di un post: id, contenuto, immagine,
+autore (nome, avatar), numero di likes, data creazione.
+Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like. */
+
+const socialPost = [
+    post = {
+        author: "",
+        content: "",
+        img: "",
+        like: 0,
+        date: "",
+
+    },
+    post = {
+        author: "",
+        content: "",
+        img: "",
+        like: 0,
+        date: "",
+
+    },
+    post = {
+        author: "",
+        content: "",
+        img: "",
+        like: 0,
+        date: "",
+
+    },
+    post = {
+        author: "",
+        content: "",
+        img: "",
+        like: 0,
+        date: "",
+
+    }
+]
