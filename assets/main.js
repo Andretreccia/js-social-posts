4cf3885c1de25412ceb34f4a21ae7073a9e4cc31
Andretreccia <andreabtreccia@gmail.com>
2021-10-25 16:23:34 +0200

    Creazine post nell'array

--- assets/main.js
+++ assets/main.js
@@ -4,37 +4,40 @@ Gli attributi minimi del modello di un post: id, contenuto, immagine,
 autore (nome, avatar), numero di likes, data creazione.
 Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like. */
 
+
 const socialPost = [
-    post = {
-        author: "",
-        content: "",
-        img: "",
+    {
+        id: 1,
+        author: {
+            nome: "Andrea",
+            avatarImg: "https://unsplash.it/100/100?image=",
+        },
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis enim hic voluptatem maxime exercitationem dolorem quod tenetur optio molestiae fuga laudantium, dolores perspiciatis porro vitae inventore dicta totam, sed quia?",
+        img: "https://unsplash.it/600/400?image=",
         like: 0,
-        date: "",
-
+        date: "25/10/2021",
     },
-    post = {
-        author: "",
-        content: "",
-        img: "",
+    {
+        id: 2,
+        author: {
+            nome: "Luca",
+            avatarImg: "https://unsplash.it/100/100?image=",
+        },
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis enim hic voluptatem maxime exercitationem dolorem quod tenetur optio molestiae fuga laudantium, dolores perspiciatis porro vitae inventore dicta totam, sed quia?",
+        img: "https://unsplash.it/600/400?image=",
         like: 0,
-        date: "",
-
+        date: "24/10/2021",
     },
-    post = {
-        author: "",
-        content: "",
-        img: "",
+    {
+        id: 3,
+        author: {
+            nome: "Paolo",
+            avatarImg: "https://unsplash.it/100/100?image=",
+        },
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis enim hic voluptatem maxime exercitationem dolorem quod tenetur optio molestiae fuga laudantium, dolores perspiciatis porro vitae inventore dicta totam, sed quia?",
+        img: "https://unsplash.it/600/400?image=",
         like: 0,
-        date: "",
-
-    },
-    post = {
-        author: "",
-        content: "",
-        img: "",
-        like: 0,
-        date: "",
-
+        date: "18/10/2021",
     }
 ]
+
